525f431618b493724a71a101498924cbf6618f59
Eduardo Ferraz <57379069+edupferraz@users.noreply.github.com>
2020-12-30 15:25:45 -0300

    Modificação para funções e ajustes no menu

--- main.py
+++ main.py
@@ -4,38 +4,31 @@ print("Calc! - Calculadora Online para todas as coisa.")
 print()
 
 print("Matemática")
-print("    [1.1] Equação do 1º Grau")
-print("    [1.2] Equação do 2º Grau")
-print("    [1.3] Área do Quadrado")
+print("    [1] Equação do 1º Grau")
+print("    [2] Equação do 2º Grau")
+print("    [3] Área do Quadrado")
 print("Física")
-print("    [2.1] Velocidade Média")
-print("    [2.2] Movimento Uniforme")
-print("    [2.3] Equação de Torricelli")
+print("    [4] Velocidade Média")
+print("    [5] Movimento Uniforme")
+print("    [6] Equação de Torricelli")
 
 print()
 
 escolha = input("Escolha a equação desejada: ")
 print()
 
-if escolha == "1.1":
-    print("Exemplo: ax + b = 0")
-
-    a = input("Digite o a: ")
-    b = input("Digite o b: ")
-    x = input("Digite o x: ")
-
-    print()
-
+def carregando():
     print("Pro")
-    t.sleep(1)
+    t.sleep(0.5)
     print("ce")
-    t.sleep(1)
+    t.sleep(0.5)
     print("ssan")
-    t.sleep(1)
+    t.sleep(0.5)
     print("do !!!!!")
-    t.sleep(1)
+    t.sleep(0.5)
     print()
 
+def funcao_1_grau():
     if a == "" and b != "" and x != "":
         print(f"x.{x} + {b} = 0")
         print(f"x.{x} =  -{b}")
@@ -56,3 +49,17 @@ if escolha == "1.1":
         print(f"{int(a) * int(x) + int(b)} = 0")
     else:
         print("Digite parâmetros válidos")
+
+if escolha == "1":
+    print("Exemplo: ax + b = 0")
+
+    a = input("Digite o a: ")
+    b = input("Digite o b: ")
+    x = input("Digite o x: ")
+
+    print()
+    carregando()
+
+    funcao_1_grau()
+
+
